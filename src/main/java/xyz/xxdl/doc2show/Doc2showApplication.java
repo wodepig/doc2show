033551c33333e31352e612d4f28d601534ab11d7
aigo_git <aigo_git@xxdl.top>
2023-07-30 21:24:42 +0800

    init

---
+++ src/main/java/xyz/xxdl/doc2show/Doc2showApplication.java
@@ -0,0 +1,13 @@
+package xyz.xxdl.doc2show;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Doc2showApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Doc2showApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/xxdl/doc2show/Doc2showApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.xxdl.doc2show;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Doc2showApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
